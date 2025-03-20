5eec233ef69b96cab65f8bf847fabb2d23298336
ericcheung1 <ericcheun251@gmail.com>
2025-03-19 19:27:14 -0700

    added file and modified comments

--- nba_proj/player_ids.py
+++ nba_proj/player_ids.py
@@ -6,10 +6,10 @@ def get_player_ids(name, db_path):
     if player has an associated player_id returns player_id,
     if not adds player to players table then generates and return player_id
     """
-    # connecting to database
+    # connecting to database, queries for chosen player using parameterized query
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor() # creates cursor object to excecute queries
-    cursor.execute('SELECT player from players WHERE player = ?', (name,)) # queries for chosen player
+    cursor.execute('SELECT player from players WHERE player = ?', (name,)) # query for chosen player
     result = cursor.fetchone() # fetches the first result from query
 
     try:
@@ -21,7 +21,7 @@ def get_player_ids(name, db_path):
             max_id = cursor.fetchone()[0] # fetches the highest/newest player_id
             player_id = 1 if max_id is None else max_id+1 # assigns first or next player_id
 
-            # inserting player_id and name into players table
+            # inserting player_id and name into players table, using parameterized query
             cursor.execute('INSERT INTO players (player_id, player) VALUES (?, ?)', (player_id, name))
             conn.commit()
             return player_id
@@ -30,7 +30,7 @@ def get_player_ids(name, db_path):
         print(f'Database Error: {e}')
         return None
     
-    finally: # closing curser and connection
+    finally: # closing cursor and connection
         if cursor:
             cursor.close()
         if conn:
